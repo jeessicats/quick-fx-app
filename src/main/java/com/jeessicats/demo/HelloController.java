48efd42be1883119e30a7e4f551ce3c947c0dd99
Jessica <jeessicats@gmail.com>
2024-12-13 14:28:15 -0300

    Limitações em TextFields e Interface Initializable no JavaFX

--- src/main/java/com/jeessicats/demo/HelloController.java
+++ src/main/java/com/jeessicats/demo/HelloController.java
@@ -1,15 +1,19 @@
 package com.jeessicats.demo;
 
 import com.jeessicats.demo.util.Alerts;
+import com.jeessicats.demo.util.Constraints;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 
+import java.net.URL;
 import java.util.Locale;
+import java.util.ResourceBundle;
 
-public class HelloController {
+public class HelloController implements Initializable {
 
     @FXML
     private TextField textNumber1;
@@ -30,9 +34,17 @@ public class HelloController {
             double number1 = Double.parseDouble(textNumber1.getText());
             double number2 = Double.parseDouble(textNumber2.getText());
             double sum = number1 + number2;
-            labelResult.setText(String.format("%.2f", sum));
+            labelResult.setText(String.format(Locale.getDefault(), "%.2f", sum));
         } catch (NumberFormatException e) {
-            Alerts.showAlert("Error", "Parse error", e.getMessage(), Alert.AlertType.ERROR);
+            Alerts.showAlert("Error", "Parse error", "Invalid number format: " + e.getMessage(), Alert.AlertType.ERROR);
         }
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        Constraints.setTextFieldDouble(textNumber1);
+        Constraints.setTextFieldDouble(textNumber2);
+        Constraints.setTextFieldMaxLength(textNumber1, 15);
+        Constraints.setTextFieldMaxLength(textNumber2, 15);
     }
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module com.jeessicats.demo {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.desktop;
 
 
     opens com.jeessicats.demo to javafx.fxml;
