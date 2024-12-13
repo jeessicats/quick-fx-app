9da89dc97d397151a652f810f3417805cac991ec
Jessica <jeessicats@gmail.com>
2024-12-13 14:07:17 -0300

    first commit

--- src/main/java/com/jeessicats/demo/HelloController.java
+++ src/main/java/com/jeessicats/demo/HelloController.java
@@ -1,14 +1,38 @@
 package com.jeessicats.demo;
 
+import com.jeessicats.demo.util.Alerts;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+import java.util.Locale;
 
 public class HelloController {
-    @FXML
-    private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private TextField textNumber1;
+
+    @FXML
+    private TextField textNumber2;
+
+    @FXML
+    private Label labelResult;
+
+    @FXML
+    private Button buttonSum;
+
+    @FXML
+    public void onButtonSumClicked() {
+        try {
+            Locale.setDefault(Locale.US);
+            double number1 = Double.parseDouble(textNumber1.getText());
+            double number2 = Double.parseDouble(textNumber2.getText());
+            double sum = number1 + number2;
+            labelResult.setText(String.format("%.2f", sum));
+        } catch (NumberFormatException e) {
+            Alerts.showAlert("Error", "Parse error", e.getMessage(), Alert.AlertType.ERROR);
+        }
     }
 }
--- src/main/java/com/jeessicats/demo/util/Alerts.java
+++ src/main/java/com/jeessicats/demo/util/Alerts.java
@@ -1,4 +1,14 @@
 package com.jeessicats.demo.util;
 
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+
 public class Alerts {
+    public static void showAlert(String title, String header, String content, AlertType type) {
+        Alert alert = new Alert(type);
+        alert.setTitle(title);
+        alert.setHeaderText(header);
+        alert.setContentText(content);
+        alert.show();
+    }
 }
